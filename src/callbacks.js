92f7644b3677a69ade598ec0280c232b52caec91
Tyson Williams <tyson.j.williams@gmail.com>
2018-01-10 11:00:24 -0700

    Update 'contains' problem to exlude es6 includes

--- src/callbacks.js
+++ src/callbacks.js
@@ -28,7 +28,9 @@ const multiplyNums = (x, y, cb) => {
 const contains = (item, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  cb(list.includes(item));
+  for (let i = 0; i < list.length; i++) {
+    if (item === list[i]) return cb(true);
+  } return cb(false);
 };
 
 /* STRETCH PROBLEM */
